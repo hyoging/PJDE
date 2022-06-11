0ec0bd9ea067542ce050d6aec45d892e9fe079ea
hyoging <e-mirim.hs.kr>
2022-06-11 16:20:36 +0900

    node server

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express')
+
+var app = express()
+
+app.listen(8080, function() {
+    console.log("서버 실행중...")
+})
+
+app.get('/', function(req,res) {
+    res.sendFile(__dirname + "/public/login.html")
+})
+
+app.get('/main', function(req,res) {
+    res.sendFile(__dirname + "/public/login.html")
+})
+
+app.use(express.static('public'))
