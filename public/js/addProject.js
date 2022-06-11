af5f9adeed2dcee1e5970560a9813da67377c6a6
plmqazoknijb <w2033@e-mirim.hs.kr>
2022-06-12 02:14:44 +0900

    addProject - color 선택으로 변경

--- public/js/addProject.js
+++ public/js/addProject.js
@@ -1,3 +1,18 @@
+
+//사진 선택 기능
+function colorChange(){
+    // var color = document.getElementById("selectColor").value;
+    // document.getElementById("div1").style.backgroundColor = color;
+
+    var color = document.getElementById("selectColor");
+    var div = document.getElementById("div1");
+
+    console.log(selectColor);
+    console.log(selectColor.value);
+
+    div.style.backgroundColor = selectColor.value;
+}
+
 //멤버 추가, 삭제 기능
 var arrInput = new Array(0);
 var arrInputValue = new Array(0);
@@ -31,52 +46,16 @@ if (arrInput.length > 0) {
 display(); 
 }
 
-
-// 이미지 추가 기능
-  $(document).ready(function(){
- var fileTarget = $('.filebox .upload-hidden');
-
-  fileTarget.on('change', function(){
-      if(window.FileReader){
-          // 파일명 추출
-          var filename = $(this)[0].files[0].name;
-      } 
-
-      else {
-          // Old IE 파일명 추출
-          var filename = $(this).val().split('/').pop().split('\\').pop();
-      };
-
-      $(this).siblings('.upload-name').val(filename);
-  });
-
-  //preview image 
-  var imgTarget = $('.preview-image .upload-hidden');
-
-  imgTarget.on('change', function(){
-      var parent = $(this).parent();
-      parent.children('.upload-display').remove();
-
-      if(window.FileReader){
-          //image 파일만
-          if (!$(this)[0].files[0].type.match(/image\//)) return;
-          
-          var reader = new FileReader();
-          reader.onload = function(e){
-              var src = e.target.result;
-              parent.prepend('<div class="upload-display"><div class="upload-thumb-wrap"><img src="'+src+'" class="upload-thumb"></div></div>');
-          }
-          reader.readAsDataURL($(this)[0].files[0]);
-      }
-
-      else {
-          $(this)[0].select();
-          $(this)[0].blur();
-          var imgSrc = document.selection.createRange().text;
-          parent.prepend('<div class="upload-display"><div class="upload-thumb-wrap"><img class="upload-thumb"></div></div>');
-
-          var img = $(this).siblings('.upload-display').find('img');
-          img[0].style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(enable='true',sizingMethod='scale',src=\""+imgSrc+"\")";        
-      }
-  });
-});
+// function createDiv() {
+//     // 1. <div> element 만들기
+//     const newDiv = document.createElement('div');
+    
+//     // 2. <div>에 들어갈 text node 만들기
+//     const newText = document.createTextNode('안녕하세요');
+    
+//     // 3. <div>에 text node 붙이기
+//     newDiv.appendChild(newText);
+    
+//     // 4. <body>에 1에서 만든 <div> element 붙이기
+//     document.body.appendChild(newDiv);
+//   } 
