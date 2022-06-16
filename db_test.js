efda4133576387c47a8435c02c692ae8272fd2ee
hyoging <e-mirim.hs.kr>
2022-06-16 14:05:09 +0900

    first

---
+++ db_test.js
@@ -0,0 +1,23 @@
+var connection = mysql.createConnection(conn); // DB 커넥션 생성
+connection.connect();   // DB 접속
+ 
+var testQuery = "INSERT INTO `members` (`username`,`password`) VALUES ('test','test');";
+ 
+connection.query(testQuery, function (err, results, fields) { // testQuery 실행
+    if (err) {
+        console.log(err);
+    }
+    console.log(results);
+});
+ 
+testQuery = "SELECT * FROM MEMBERS";
+ 
+connection.query(testQuery, function (err, results, fields) { // testQuery 실행
+    if (err) {
+        console.log(err);
+    }
+    console.log(results);
+});
+ 
+ 
+connection.end(); // DB 접속 종료
---
+++ pjde.js
@@ -0,0 +1,8 @@
+const mysql = require('mysql');  // mysql 모듈 로드
+const conn = {  // mysql 접속 설정
+    host: '127.0.0.1',
+    port: '3306',
+    user: 'root',
+    password: 'hyojin0712',
+    database: 'monolithic'
+};
